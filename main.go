71872684c292831e1875e79ec3649fe78cda0ef4
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-08-03 04:12:06 +0700

    change metric route, now serve html file

--- main.go
+++ main.go
@@ -18,9 +18,16 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html;")
 	w.WriteHeader(http.StatusOK)
-	w.Write(fmt.Appendf([]byte{}, "Hits: %d", cfg.fileserverHits.Load()))
+	w.Write(fmt.Appendf([]byte{}, `
+	<html>
+	<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+	</body>
+	</html>`,
+		cfg.fileserverHits.Load()))
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
