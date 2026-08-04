b2fe596bc1790920760e0f18cafcaf0e658f46a1
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-08-04 11:27:46 +0700

    add get chirps API

--- main.go
+++ main.go
@@ -75,6 +75,7 @@ func main() {
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerCreateChirp)
+	mux.HandleFunc("GET /api/chirps", apiCfg.handlerGetChirps)
 
 	s := http.Server{
 		Handler: mux,
