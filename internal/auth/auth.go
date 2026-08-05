e01ee2c11f2bda2708e8b7610e84e1396b7d0f0c
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-08-05 09:12:17 +0700

    function to generate JWT

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,13 @@
 package auth
 
-import "github.com/alexedwards/argon2id"
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+
+	"github.com/alexedwards/argon2id"
+)
 
 func HashPassword(password string) (string, error) {
 	return argon2id.CreateHash(password, argon2id.DefaultParams)
@@ -8,4 +15,16 @@ func HashPassword(password string) (string, error) {
 
 func CheckPasswordHash(password, hash string) (bool, error) {
 	return argon2id.ComparePasswordAndHash(password, hash)
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	current_time := time.Now().UTC()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy-access",
+		IssuedAt:  jwt.NewNumericDate(current_time),
+		ExpiresAt: jwt.NewNumericDate(current_time.Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+
+	return token.SignedString([]byte(tokenSecret))
 }
