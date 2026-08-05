ad3523b52c4585b2b5d2c471074a4ce01ee69ee3
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-08-05 08:26:43 +0700

    helper function to compare hash password

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -5,3 +5,7 @@ import "github.com/alexedwards/argon2id"
 func HashPassword(password string) (string, error) {
 	return argon2id.CreateHash(password, argon2id.DefaultParams)
 }
+
+func CheckPasswordHash(password, hash string) (bool, error) {
+	return argon2id.ComparePasswordAndHash(password, hash)
+}
