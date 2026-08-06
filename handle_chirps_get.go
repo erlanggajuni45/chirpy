62f8b14c323286cf6f7072dd76a2d3b8e344dd24
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-08-06 14:06:12 +0700

    result get chirps can be order by sort query

--- handle_chirps_get.go
+++ handle_chirps_get.go
@@ -5,12 +5,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"sort"
 
+	"github.com/erlanggajuni45/chirpy/internal/database"
 	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 	author := r.URL.Query().Get("author_id")
+
+	var chirps = make([]database.Chirp, 0)
+	var err error
+
 	if author != "" {
 		userId, err := uuid.Parse(author)
 		if err != nil {
@@ -19,40 +25,31 @@ func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		chirps, err := cfg.database.GetChirpsByUser(r.Context(), userId)
+		chirps, err = cfg.database.GetChirpsByUser(r.Context(), userId)
 		if err != nil {
 			fmt.Printf("Error when getting chirps by user id: %v\n", err)
 			w.WriteHeader(500)
 			return
 		}
+	} else {
+		chirps, err = cfg.database.GetChirps(r.Context())
 
-		var chirpList = make([]Chirp, 0)
-		for _, c := range chirps {
-			chirpList = append(chirpList, Chirp{
-				ID:        c.ID,
-				CreatedAt: c.CreatedAt,
-				UpdatedAt: c.UpdatedAt,
-				Body:      c.Body,
-				UserID:    c.UserID,
-			})
+		if err != nil {
+			fmt.Printf("Error when getting chirps: %v\n", err)
+			w.WriteHeader(500)
+			w.Write([]byte("Error when getting chirps: " + err.Error()))
+			return
 		}
+	}
 
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(200)
-		json.NewEncoder(w).Encode(chirpList)
-		return
+	sortQuery := r.URL.Query().Get("sort")
+	if sortQuery == "desc" {
+		sort.Slice(chirps, func(i, j int) bool {
+			return chirps[i].CreatedAt.After(chirps[j].CreatedAt)
+		})
 	}
 
 	var chirpList = make([]Chirp, 0)
-	chirps, err := cfg.database.GetChirps(r.Context())
-
-	if err != nil {
-		fmt.Printf("Error when getting chirps: %v\n", err)
-		w.WriteHeader(500)
-		w.Write([]byte("Error when getting chirps: " + err.Error()))
-		return
-	}
-
 	for _, c := range chirps {
 		chirpList = append(chirpList, Chirp{
 			ID:        c.ID,
